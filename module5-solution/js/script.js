f43d385bcc002963f176cdf01f6629f307a56e2e
Sayali Kadam <kadam.sayali@gmail.com>
2022-01-18 22:13:59 +0530

    submition

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -123,11 +123,11 @@ function buildAndShowHomeHTML (categories) {
       
       finalHtml += homeHtmlToInsertIntoMainPage;
       finalHtml += "</section>";
-      
+
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("#main-content", finalHtml); 
+      //insertHtml("#main-content", finalHtml); 
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
